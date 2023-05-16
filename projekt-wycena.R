816ec6b3f0c9e834647d9d2c31479f3dc4540151
klaudia-tunska <102724307+klaudia-tunska@users.noreply.github.com>
2023-05-16 20:14:22 +0200

    Update projekt-wycena.R

--- projekt-wycena.R
+++ projekt-wycena.R
@@ -5,11 +5,56 @@
 # robimy funkcje payoff dla call i put
 
 
-dt<-1/12
-sigma<-0.3
-u<-exp(sigma*sqrt(dt))
-d<-exp(-sigma*sqrt(dt))
-so<-50
-r<-0.02
-k<-48
-t<-2
+k <- 24 # rozmiar macierzy (później = 24)
+A <- matrix(NA, k, k)
+A[k, 1] <- 1
+for (i in 2:k){
+  for (j in (k - i + 1):k){
+    if (j == k) {
+      A[j, i] <- d * A[j, i - 1]
+    }
+    else {
+      A[j, i] <- u * A[j + 1, i - 1]
+    }
+  }
+}
+
+
+model <- function(S_0, u, d, r, K, d_t, T){
+  
+  k <- T / d_t    # liczba kroków w drzewie, rozmiar macierzy
+  A <- matrix(NA, k, k)
+  A[k, 1] <- 1
+  for (i in 2:k){
+    for (j in (k - i + 1):k){
+      if (j == k) {
+        A[j, i] <- d * A[j, i - 1]    # krok w dół (ale stosujemy tylko przy ostatniej wartości w momencie t)
+      }
+      else {
+        A[j, i] <- u * A[j + 1, i - 1]    # krok w górę
+      }
+    }
+  }
+  S_T = S_0 * A
+  
+  return(S_T)
+
+
+}
+
+ 
+
+
+# nasze dane
+d_t <- 1 / 12
+sigma <- 0.3
+u <- exp(sigma * sqrt(d_t))
+d <- exp(-sigma * sqrt(d_t))
+S_0 <- 50
+r <- 0.02
+K <- 48
+T <- 2
+
+ 
+
+S_T <- model(S_0, u, d, r, K, d_t, T)
