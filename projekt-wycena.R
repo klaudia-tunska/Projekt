cb46a19818523dab3cdc8728ffc333a835c30f3a
Michał Marmol <64933687+ometiclvo@users.noreply.github.com>
2023-05-20 16:06:28 +0200

    Update projekt-wycena.R
    drzewo takim sposobem z jedną pętlą jak na tablicy, porównałem wyniki z tym co mieliśmy na dwie pętle i wychodzi tak samo

--- projekt-wycena.R
+++ projekt-wycena.R
@@ -6,7 +6,7 @@
 
 
 # nasze dane
-d_t <- 1 / 3
+d_t <- 1 / 12
 sigma <- 0.3
 u <- exp(sigma * sqrt(d_t))
 d <- exp(-sigma * sqrt(d_t))    # d = 1 / u = u**(-1)
@@ -16,26 +16,16 @@ K <- 48
 T <- 2
 
 
-model <- function(S_0, u, d, r, K, d_t, T){
+binomial_tree <- function(S_0, u, d, r, K, d_t, T){
   
-  k <- T / d_t    # liczba kroków w drzewie, rozmiar macierzy
-  A <- matrix(NA, k, k)
-  A[k, 1] <- 1
-  for (i in 2:k){
-    for (j in (k - i + 1):k){
-      if (j == k) {
-        A[j, i] <- d * A[j, i - 1]    # krok w dół (ale stosujemy tylko przy ostatniej wartości w momencie t)
-      }
-      else {
-        A[j, i] <- u * A[j + 1, i - 1]    # krok w górę
-      }
-    }
+  N <- T / d_t    # liczba kroków w drzewie, rozmiar macierzy
+  A <- matrix(NA, N, N)
+  for (i in 1:N){
+    A[N:(N - i + 1), i] <- u**(seq(-(i - 1), (i - 1), 2))
   }
   S_T = S_0 * A
   
   return(S_T)
-
-
 }
 
 
