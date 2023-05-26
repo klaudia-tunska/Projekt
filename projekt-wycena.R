9b369c5c62ad63bd485d33e507363623db37a9b2
Michał Marmol <64933687+ometiclvo@users.noreply.github.com>
2023-05-26 23:16:52 +0200

    Update projekt-wycena.R

--- projekt-wycena.R
+++ projekt-wycena.R
@@ -19,14 +19,14 @@ N <- matrix_size(d_t, T)
 
 binomial_tree <- function(S_0, u, d_t, T){
   
-  N <- matrix_size(d_t, T)    # liczba kroków w drzewie, rozmiar macierzy
-  A <- matrix(NA, nrow = N, ncol = N)
-  
-  sapply(1:N, function(i) {
+  N <- T / d_t +1  # liczba kroków w drzewie, rozmiar macierzy(dodajemy 1 gdyż R nie liczy od 0)
+  A <- matrix(NA, N, N)
+  for (i in 1:N){
     A[N:(N - i + 1), i] <- u**(seq(-(i - 1), (i - 1), 2))
-  })
+  }
+  S_T <- S_0 * A
   
-  return(S_0 * A)
+  return(S_T)
 }
 
 S_T <- binomial_tree(S_0, u, d_t, T)
